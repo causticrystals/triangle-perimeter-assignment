f07fa230e2c40c2a4cb91b155e4768b84de4dfa3
causticrystals <peanutpc173@gmail.com>
2022-09-12 20:42:42 -0600

    Update main.js

--- main.js
+++ main.js
@@ -15,9 +15,16 @@ function btnClicked() {
     document.getElementById("aBOutp").innerHTML = dist(xAInput, yAInput, xBInput, yBInput);
     document.getElementById("aCOutp").innerHTML = dist(xAInput, yAInput, xCInput, yCInput);
     document.getElementById("bCOutp").innerHTML = dist(xBInput, yBInput, xCInput, yCInput);
-    document.getElementById("abcPerimeter").innerHTML = dist();
+
+    // // Uhh whole perimeter stuff
+    // let sideAB = +document.getElementById("aBOutp").value
+    // let sideAC = +document.getElementById("aCOutp").value
+    // let sideBC = +document.getElementById("bCOutp").value
+    // document.getElementById("abcPerimeter").innerHTML = (sideAB + sideAC + sideBC);
 }
 
 function dist(x1, y1, x2, y2) {
-    Math.sqrt(x1 ** 2 - x2 ** 2) + Math.sqrt(y1 ** 2 - y2 ** 2);
+    let sideCalc = Math.sqrt(x1 ** 2 - x2 ** 2) + Math.sqrt(y1 ** 2 - y2 ** 2);
+    return sideCalc;
 }
+
